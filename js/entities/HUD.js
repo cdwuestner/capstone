aac58101c2ee0306b5febd978fb3a7587e3b9926
Chris Wuestner <cdwuestner@gmail.com>
2017-06-04 01:53:10 -0500

    Added instructional text to HUD

--- js/entities/HUD.js
+++ js/entities/HUD.js
@@ -13,22 +13,23 @@ game.HUD.Container = me.Container.extend({
         this.name = "HUD";
         // Add stored unit graphic as child
         this.addChild(new game.HUD.BuildUnits(0, 0));
+        this.addChild(new game.HUD.UnitInstructions(0, 0));
     }
 });
 // Display how many units can be "built"
-game.HUD.BuildUnits = me.Renderable.extend( {
+game.HUD.BuildUnits = me.Renderable.extend({
     // Constructor
     init : function (x, y) {
-    // Call parent constructor
-    this._super(me.Renderable, 'init', [x, y, 10, 10]);
-    // Create font
-    this.color = me.pool.pull("me.Color", 255, 255, 255);
-    this.font = new me.Font("Arial", 25, this.color);
-    // Align font
-    this.font.textAlign = "left";
-    this.font.textBaseline = "top";
-    // Local copy of stored units
-    this.storedUnits = 0;
+        // Call parent constructor
+        this._super(me.Renderable, 'init', [x, y, 10, 10]);
+        // Create font (white Arial)
+        this.color = me.pool.pull("me.Color", 255, 255, 255);
+        this.font = new me.Font("Arial", 25, this.color);
+        // Align font
+        this.font.textAlign = "left";
+        this.font.textBaseline = "top";
+        // Local copy of stored units
+        this.storedUnits = 0;
     },
     // Update: return true if stored units changes
     update : function (dt) {
@@ -38,8 +39,29 @@ game.HUD.BuildUnits = me.Renderable.extend( {
         }
         return false;
     },
-    // Draw stored units render
+    // Draw available units render
     draw : function (renderer) {
-	    this.font.draw (renderer, "Stored Units: " + this.storedUnits, 0, 30);
+	    this.font.draw(renderer, "Available Units: " + this.storedUnits, 165, 30);
+    }
+});
+// Give the user instructions
+game.HUD.UnitInstructions = me.Renderable.extend({
+    // Constructor
+    init : function(x, y){
+        // Call parent constructor
+        this._super(me.Renderable, 'init', [x, y, 10, 10]);
+        // Create font (white Arial)
+        this.color = me.pool.pull("me.Color", 255, 255, 255);
+        this.font = new me.Font("Arial", 15, this.color);
+        // Align font
+        this.font.textAlign = "left";
+        this.font.textBaseline = "top";
+    },
+    // Draw the instructional text
+    draw : function (renderer) {
+        this.font.draw(renderer, "Press:", 400, 20);
+        this.font.draw(renderer, "W for a Wizard\n"
+                + " E for a Healer\n"
+                + " R for a Warrior", 450, 20);
     }
 });
